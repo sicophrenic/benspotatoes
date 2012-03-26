cab6abbb5f07e1d63983d192dcb3020ebe96e89b
Benjamin Hsieh <bhsiehh@gmail.com>
2012-03-26 16:55:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# benspotatoes
+## rottentomatoes >> rottenpotatoes >> benspotatoes
+
+ben's internal movie tracking database
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benspotatoes::Application.config.secret_token = 'c299a124cc4dc8516986f88968b009b866457a4fc01da03f8f69a7d063dfd7bd4eb33b7441208d0d1c3477e6bfb8be3b77b4a41007b7c51009ede877adfa3a29'
