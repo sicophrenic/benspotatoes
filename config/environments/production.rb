3096266fb913d670df2f2cb78367ec23f9f12027
Benjamin Hsieh <bhsiehh@gmail.com>
2012-03-27 01:54:38 -0700

    precompiled assets again with custom.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Benspotatoes::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.precompile += %w( custom.css )
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
