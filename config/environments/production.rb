fc0f35ce0377061d8284fef1c391e7a0087da900
Benjamin Hsieh <bhsiehh@gmail.com>
2012-04-16 13:10:29 -0700

    precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Benspotatoes::Application.configure do
 
   config.assets.precompile += %w( custom.css )
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
