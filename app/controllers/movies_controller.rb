3b23c09b2b228b515e6fc582299810f34c84499c
Benjamin Hsieh <bhsiehh@gmail.com>
2012-04-25 18:32:19 -0700

    DRY views, removed flash messages, minor #movielist row modification

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     
     if params[:nuke]
       session.clear
-        flash[:notice] = "Session reset."
+        # flash[:notice] = "Session reset."
         redirect_to movies_path()
     end
     
@@ -78,7 +78,7 @@ class MoviesController < ApplicationController
     
     if @paginate
       if @search != ''
-        flash.now[:notice] = %Q(Search for "#{@search}")
+        # flash.now[:notice] = %Q(Search for "#{@search}")
         @movies = Movie.find(:all,  :conditions => ["rating IN (?) AND
                                                     location IN (?) AND
                                                     quality IN (?) AND
@@ -97,7 +97,7 @@ class MoviesController < ApplicationController
       end
     else
       if @search != ''
-        flash.now[:notice] = %Q(Search for "#{@search}")
+        # flash.now[:notice] = %Q(Search for "#{@search}")
         @movies = Movie.find(:all,  :conditions => ["rating IN (?) AND
                                                     location IN (?) AND
                                                     quality IN (?) AND
